e393455ab26ffd2ac47419f9a0b31d4f60333e2c
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-01-25 12:35:51 +0100

    Bugfix for the CI script that searches for trailing newlines
    Let's assume that there are multiple file without a trailing newline. As
    expected, the CI script finds these files. But the error message was not
    well formatted. It contained to much new lines... (Have you noticed the
    irony...)
    
    That issue was resolved.

--- ci/20-trailing_newline.sh
+++ ci/20-trailing_newline.sh
@@ -14,8 +14,9 @@ FOUND=0
 for FILE in $(find $FOLDER -regex $FILES); do
     LASTLINE=$(tail -n 1 $FILE; echo x)
     LASTLINE=${LASTLINE%x}
-    if [ "${LASTLINE: -1}" != $'\n' ] ; then
-        echo -e "\nFound:\t$FILE"
+    if [ "${LASTLINE: -1}" != $'\n' ]; then
+        [ $FOUND == 0 ] && echo -e "\tError."
+        echo -e "Found:\t$FILE"
         FOUND=1
     fi
 done
