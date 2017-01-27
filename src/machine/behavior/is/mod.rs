3a212bbb255f33fc4b4fbd449e2901e47eff5cc7
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-01-27 19:21:32 +0100

    Mapping from instruction opcodes to semantics specified

--- src/machine/behavior/is/mod.rs
+++ src/machine/behavior/is/mod.rs
@@ -10,3 +10,24 @@ pub mod memory;
 pub mod other;
 pub mod subroutine;
 pub mod system;
+
+use machine::state::State;
+
+pub fn get(_opcode: u8) -> Semantic {
+    unimplemented!();
+    // SEMANTICS[opcode]
+}
+
+type Semantic = fn(&mut State, u8, u8, u8);
+/*
+pub const SEMANTICS: [Semantic; 256] = [
+                        // symbol   hex  dec
+
+    interrupt::trap,    // TRAP     #00    0
+    float::fcmp,        // FCMP     #01    1
+
+    /* TODO */          // PUSHGOI  #BF  191
+
+    other::swym         // SWYM     #FF  255
+];
+*/
