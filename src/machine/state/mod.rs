58e129f19410db7927b506107ebdc24a2d05c221
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-01-27 15:15:22 +0100

    Struct State defined

--- src/machine/state/mod.rs
+++ src/machine/state/mod.rs
@@ -3,3 +3,20 @@ pub mod gpr;
 pub mod mem;
 pub mod pc;
 pub mod sr;
+
+use self::sr::SRegisters;
+use self::gpr::GPRegisters;
+use self::mem::Memory;
+
+pub struct State {
+    pc: u64,
+    sr: SRegisters,
+    gpr: GPRegisters,
+    mem: Memory,
+}
+
+impl State {
+    fn new() -> Self {
+        unimplemented!();
+    }
+}
