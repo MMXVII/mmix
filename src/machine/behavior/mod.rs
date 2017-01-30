f402709d099ddc881811d9053fd88384ff5a47e0
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-01-30 13:39:19 +0100

    Trailing whitespace removed

--- src/machine/behavior/mod.rs
+++ src/machine/behavior/mod.rs
@@ -3,7 +3,7 @@
 //! This modules provides any functionality that belongs to the behavior of an MMIX. This includes
 //! the instruction set which can be found in a submodule called `is` and code that emulates the
 //! control unit (including the von Neumann cycle or a comparable architecture) grouped in a
-//! submodule `cu`. 
+//! submodule `cu`.
 
 pub mod cu;
 pub mod is;
