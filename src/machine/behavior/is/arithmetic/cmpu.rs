99e0a0d21112c626a3301425ad10062e23cfd136
Janina Born <jaborn@uni-osnabrueck.de>
2017-02-02 21:43:26 +0100

    Bugfix

--- src/machine/behavior/is/arithmetic/cmpu.rs
+++ src/machine/behavior/is/arithmetic/cmpu.rs
@@ -5,11 +5,11 @@ pub fn cmpu(state: &mut State, x: u8, y: u8, z: u8) {
     let op1: u64 = state.gpr[y].into();
 	let op2: u64 = state.gpr[z].into();
 	// execute 
-    let dif = op1.wrapping_add(op2);
+    let dif = op1.wrapping_sub(op2);
     // store result
-    if x > 0 {
+    if dif > 0 {
     	state.gpr[x] = (1 as u64).into();
-    } else if x == 0 {
+    } else if dif == 0 {
     	state.gpr[x] = (0 as u64).into();
     } else {
     	state.gpr[x] = (-1 as i64).into();
