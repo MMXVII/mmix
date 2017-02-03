8d54a3f6a064bca2f4038b4d87e97e860295c252
Janina Born <janina.born.1995@gmail.com>
2017-02-03 16:37:05 +0100

    Change the instruction 'cmp'

--- src/machine/behavior/is/arithmetic/cmp.rs
+++ src/machine/behavior/is/arithmetic/cmp.rs
@@ -1,16 +1,13 @@
 use machine::state::State;
 
 pub fn cmp(state: &mut State, x: u8, y: u8, z: u8) {
-    // load operands
+    // Load operands
     let op1: i64 = state.gpr[y].into();
     let op2: i64 = state.gpr[z].into();
-    // check if op1 is greater, less or equal than op2
-    // store result
-    if op1 < op2 {
-        state.gpr[x] = (-1 as i64).into();
-    } else if op1 > op2 {
-        state.gpr[x] = (1 as i64).into();
-    } else {
-        state.gpr[x] = (0 as i64).into();
-    }
+
+    // Execute
+    let res : i64 = op1.cmp(&op2) as i64;
+
+    // Store result
+    state.gpr[x] = res.into();
 }
