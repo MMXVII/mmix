db6b4aa27d94a54fcbd03fc0aeac956f411d147e
Janina Born <janina.born.1995@gmail.com>
2017-02-03 17:49:53 +0100

    Store the remainder without using 'put'

--- src/machine/behavior/is/arithmetic/div.rs
+++ src/machine/behavior/is/arithmetic/div.rs
@@ -1,19 +1,21 @@
 use machine::state::State;
-use machine::behavior::is::other::put;
+use machine::state::sr::R;
 
 pub fn div(state: &mut State, x: u8, y: u8, z: u8) {
-    // load operands
+    // Load operands
     let op1: i64 = state.gpr[y].into();
     let op2: i64 = state.gpr[z].into();
+
+    // Execute and store results
     // check if denominator == 0
     if op2 == 0 {
-        put(state, (op1 as u8), 6, 0);
+        state.sr[R::R] = op1.into();
         state.gpr[x] = (0 as i64).into();
     } else {
-        let res = op1 / op2;
+        let res = op1.wrapping_div(op2);
         let rem = op1 - res;   // remainder
-        put(state, (rem as u8), 6, 0);
 
+        state.sr[R::R] = rem.into();
         state.gpr[x] = res.into();
     }
 }
