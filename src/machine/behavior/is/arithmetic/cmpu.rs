28ce8908b1d06badb46c17c8c00f51dbadacfc77
Janina Born <janina.born.1995@gmail.com>
2017-02-03 17:00:44 +0100

    Change the instruction 'cmpu'

--- src/machine/behavior/is/arithmetic/cmpu.rs
+++ src/machine/behavior/is/arithmetic/cmpu.rs
@@ -1,17 +1,13 @@
 use machine::state::State;
 
 pub fn cmpu(state: &mut State, x: u8, y: u8, z: u8) {
-    // load operands
+    // Load operands
     let op1: u64 = state.gpr[y].into();
     let op2: u64 = state.gpr[z].into();
-    // execute
-    let dif = op1.wrapping_sub(op2);
-    // store result
-    if dif > 0 {
-        state.gpr[x] = (1 as u64).into();
-    } else if dif == 0 {
-        state.gpr[x] = (0 as u64).into();
-    } else {
-        state.gpr[x] = (-1 as i64).into();
-    }
+
+    // Execute
+    let res: i64 = op1.cmp(&op2) as i64;
+
+    // Store results
+    state.gpr[x] = res.into();
 }
