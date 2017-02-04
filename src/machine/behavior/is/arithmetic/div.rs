12f8761739b4f87ab40f7fac227eb7f09ce3e6c1
Janina Born <janina.born.1995@gmail.com>
2017-02-04 12:03:58 +0100

    Change casting

--- src/machine/behavior/is/arithmetic/div.rs
+++ src/machine/behavior/is/arithmetic/div.rs
@@ -10,7 +10,7 @@ pub fn div(state: &mut State, x: u8, y: u8, z: u8) {
     // check if denominator == 0
     if op2 == 0 {
         state.sr[R::R] = op1.into();
-        state.gpr[x] = (0 as i64).into();
+        state.gpr[x] = 0i64.into();
     } else {
         let res = op1.wrapping_div(op2);
         let rem = op1 - res;   // remainder
