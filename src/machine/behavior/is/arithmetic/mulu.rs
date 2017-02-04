ad79920e61b65209390b8f67d1b9b97ec26b180e
Janina Born <janina.born.1995@gmail.com>
2017-02-04 12:07:20 +0100

    'Mulu' unimplemented

--- src/machine/behavior/is/arithmetic/mulu.rs
+++ src/machine/behavior/is/arithmetic/mulu.rs
@@ -1,21 +1,5 @@
 use machine::state::State;
-use machine::state::sr::R;
 
-pub fn mulu(state: &mut State, x: u8, y: u8, z: u8) {
-    // Load operands
-    let op1: u64 = state.gpr[y].into();
-    let op2: u64 = state.gpr[z].into();
-
-    // Execute
-    // lower half
-    let low_res = op1.wrapping_mul(op2);
-
-    // upper half
-    let upp_op1 = op1.wrapping_shl(32);
-    let upp_op2 = op2.wrapping_shl(32);
-    let upp_res = upp_op1.wrapping_mul(upp_op2);
-
-    // Store results
-    state.gpr[x] = low_res.into();
-    state.sr[R::H] = upp_res.into();
+pub fn mulu(_state: &mut State, _x: u8, _y: u8, _z: u8) {
+    unimplemented!();
 }
