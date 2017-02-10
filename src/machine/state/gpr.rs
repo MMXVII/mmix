904db75e476f0baa426923f42a1ea276947915f1
Karoline Plum <karoline@plum-ge.de>
2017-02-10 11:29:03 +0100

    Implement the general purpose registers

--- src/machine/state/gpr.rs
+++ src/machine/state/gpr.rs
@@ -2,23 +2,23 @@ use machine::state::types::Octa;
 
 use std::ops::{Index, IndexMut};
 
-pub struct GPRegisters(());
+pub struct GPRegisters(Vec<Octa>);
 
 impl GPRegisters {
     pub fn new() -> Self {
-        unimplemented!();
+        GPRegisters(vec![0u64.into(); 256])
     }
 }
 
 impl Index<u8> for GPRegisters {
     type Output = Octa;
-    fn index(&self, _: u8) -> &Self::Output {
-        unimplemented!();
+    fn index(&self, index: u8) -> &Self::Output {
+        self.0.index(index as usize)
     }
 }
 
 impl IndexMut<u8> for GPRegisters {
-    fn index_mut(&mut self, _: u8) -> &mut Self::Output {
-        unimplemented!();
+    fn index_mut(&mut self, index: u8) -> &mut Self::Output {
+        self.0.index_mut(index as usize)
     }
 }
