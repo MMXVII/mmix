5cf2966a00efc65cc1b834df49a91f9a34f46a39
Karoline Plum <karoline@plum-ge.de>
2017-02-10 14:02:59 +0100

    Derive Debug and PartialEq for Byte, Wyde, Tetra, Octa
    This makes testing using assert_eq! easier

--- src/machine/state/types.rs
+++ src/machine/state/types.rs
@@ -1,15 +1,15 @@
 use std::mem::transmute;
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Byte(u8);
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Wyde(u16);
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Tetra(u32);
 
-#[derive(Clone, Copy)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Octa(u64);
 
 macro_rules! type_impl {
