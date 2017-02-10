021bd2e996811e14032542c3269dea15d216a22b
BarbaraButz <bbutz@uos.de>
2017-02-10 01:11:30 +0100

    replaced duplicate code with instruction call
    in bdifi, wdifi, tdifi

--- src/machine/behavior/is/bytewise/bdifi.rs
+++ src/machine/behavior/is/bytewise/bdifi.rs
@@ -2,12 +2,5 @@ use machine::state::State;
 
 /// byte difference immediate
 pub fn bdifi(state: &mut State, x: u8, y: u8, z: u8) {
-    // Load first operand
-    let op1: u64 = state.gpr[y].into();
-
-    // Execute
-    let result = op1.saturating_sub(z as u64);
-
-    // Store result
-    state.gpr[x] = result.into();
+    odifi(x, y, z);
 }
--- src/machine/behavior/is/bytewise/tdifi.rs
+++ src/machine/behavior/is/bytewise/tdifi.rs
@@ -2,12 +2,5 @@ use machine::state::State;
 
 /// tetra difference immediate
 pub fn tdifi(state: &mut State, x: u8, y: u8, z: u8) {
-   // Load first operand
-    let op1: u64 = state.gpr[y].into();
-
-    // Execute
-    let result = op1.saturating_sub(z as u64);
-
-    // Store result
-    state.gpr[x] = result.into();
+   odifi(x, y, z);
 }
