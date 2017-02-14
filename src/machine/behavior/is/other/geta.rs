a37ab932d5f8e180354b440bbe533fd0f5a6e1c4
Janina Born <janina.born.1995@gmail.com>
2017-02-14 18:49:22 +0100

    Bugfix

--- src/machine/behavior/is/other/geta.rs
+++ src/machine/behavior/is/other/geta.rs
@@ -1,6 +1,6 @@
 use machine::state::State;
 
-pub fn geta(state: &mut State, x: u8, _y: u8, _z: u8) {
+pub fn geta(state: &mut State, x: u8, y: u8, z: u8) {
     // Load operands
     let ra: u64 = ((y as u64) << 8) + (z as u64);
     let at: u64 = state.pc.into();
