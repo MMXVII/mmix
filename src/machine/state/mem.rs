6c4e30187bd5e555f61022b273af0ab314e945bd
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-02-15 13:37:23 +0100

    Add unit tests for machine::state::mem

--- src/machine/state/mem.rs
+++ src/machine/state/mem.rs
@@ -198,3 +198,185 @@ impl IndexMut<OctaAt> for Memory {
         self.buf.index_mut(pos)
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    /// Check if any byte is accessible
+    #[test]
+    fn undersized() {
+        let mut mem = Memory::with_capacity(25);
+
+        //  0..25: regular bytes
+        // 25..32: "alignment" bytes
+        for i in 0..32 {
+            mem[ByteAt(i)] = 42u8.into();
+            let res: u8 = mem[ByteAt(i)].into();
+            assert_eq!(res, 42u8);
+        }
+    }
+
+    /// Check if the memory is not oversized
+    #[test]
+    #[should_panic]
+    fn oversized() {
+        let mut mem = Memory::with_capacity(25);
+        mem[ByteAt(32)] = 42u8.into();
+    }
+
+    /// Check if the memory is initially zeroed
+    #[test]
+    fn zeroed() {
+        let mem = Memory::with_capacity(24);
+
+        for i in 0..24 {
+            let zero: u8 = mem[ByteAt(i)].into();
+            assert_eq!(zero, 0);
+        }
+    }
+
+    /// Check if a byte is properly accessed
+    #[test]
+    fn access_byte() {
+        let mut mem = Memory::with_capacity(24);
+
+        // Write
+        mem[ByteAt(8)] = 0xFFu8.into();
+
+        // Read
+        let  byte:  u8 = mem[ ByteAt(8)].into();
+        assert_eq!( byte, 0xFF);
+
+        let  wyde: u16 = mem[ WydeAt(8)].into();
+        assert_eq!( wyde, 0xFF00);
+
+        let tetra: u32 = mem[TetraAt(8)].into();
+        assert_eq!(tetra, 0xFF00_0000);
+
+        let  octa: u64 = mem[ OctaAt(8)].into();
+        assert_eq!( octa, 0xFF00_0000_0000_0000);
+
+        // Check if anything else is zero
+        for i in (0..8).chain(9..24) {
+            let zero: u8 = mem[ByteAt(i)].into();
+            assert_eq!(zero, 0);
+        }
+    }
+
+    // Check if a wyde is properly accessed
+    #[test]
+    fn access_wyde() {
+        let mut mem = Memory::with_capacity(24);
+
+        // Write
+        mem[WydeAt(8)] = 0xFF8Fu16.into();
+
+        // Read
+        let  byte_0:  u8 = mem[ ByteAt( 8)].into();
+        let  byte_1:  u8 = mem[ ByteAt( 9)].into();
+        assert_eq!( byte_0, 0xFF);
+        assert_eq!( byte_1, 0x8F);
+
+        let  wyde  : u16 = mem[ WydeAt( 8)].into();
+        assert_eq!( wyde  , 0xFF8F);
+
+        let tetra  : u32 = mem[TetraAt( 8)].into();
+        assert_eq!(tetra  , 0xFF8F_0000);
+
+        let  octa  : u64 = mem[ OctaAt( 8)].into();
+        assert_eq!( octa  , 0xFF8F_0000_0000_0000);
+
+        // Check if anything else is zero
+        for i in (0..8).chain(10..24) {
+            let zero: u8 = mem[ByteAt(i)].into();
+            assert_eq!(zero, 0);
+        }
+    }
+
+    // Check if a tetra is properly accessed
+    #[test]
+    fn access_tetra() {
+        let mut mem = Memory::with_capacity(24);
+
+        // Write
+        mem[TetraAt(8)] = 0xFF8F_8848u32.into();
+
+        // Read
+        let  byte_0:  u8 = mem[ ByteAt( 8)].into();
+        let  byte_1:  u8 = mem[ ByteAt( 9)].into();
+        let  byte_2:  u8 = mem[ ByteAt(10)].into();
+        let  byte_3:  u8 = mem[ ByteAt(11)].into();
+        assert_eq!( byte_0, 0xFF);
+        assert_eq!( byte_1, 0x8F);
+        assert_eq!( byte_2, 0x88);
+        assert_eq!( byte_3, 0x48);
+
+        let  wyde_0: u16 = mem[ WydeAt( 8)].into();
+        let  wyde_1: u16 = mem[ WydeAt(10)].into();
+        assert_eq!( wyde_0, 0xFF8F);
+        assert_eq!( wyde_1, 0x8848);
+
+        let tetra  : u32 = mem[TetraAt( 8)].into();
+        assert_eq!(tetra  , 0xFF8F_8848);
+
+        let  octa  : u64 = mem[ OctaAt( 8)].into();
+        assert_eq!( octa  , 0xFF8F_8848_0000_0000);
+
+        // Check if anything else is zero
+        for i in (0..8).chain(12..24) {
+            let zero: u8 = mem[ByteAt(i)].into();
+            assert_eq!(zero, 0);
+        }
+    }
+
+    // Check if a octa is properly accessed
+    #[test]
+    fn access_octa() {
+        let mut mem = Memory::with_capacity(24);
+
+        // Write
+        mem[OctaAt(8)] = 0xFF8F_8848_4424_2212u64.into();
+
+        // Read
+        let  byte_0:  u8 = mem[ ByteAt( 8)].into();
+        let  byte_1:  u8 = mem[ ByteAt( 9)].into();
+        let  byte_2:  u8 = mem[ ByteAt(10)].into();
+        let  byte_3:  u8 = mem[ ByteAt(11)].into();
+        let  byte_4:  u8 = mem[ ByteAt(12)].into();
+        let  byte_5:  u8 = mem[ ByteAt(13)].into();
+        let  byte_6:  u8 = mem[ ByteAt(14)].into();
+        let  byte_7:  u8 = mem[ ByteAt(15)].into();
+        assert_eq!( byte_0, 0xFF);
+        assert_eq!( byte_1, 0x8F);
+        assert_eq!( byte_2, 0x88);
+        assert_eq!( byte_3, 0x48);
+        assert_eq!( byte_4, 0x44);
+        assert_eq!( byte_5, 0x24);
+        assert_eq!( byte_6, 0x22);
+        assert_eq!( byte_7, 0x12);
+
+        let  wyde_0: u16 = mem[ WydeAt( 8)].into();
+        let  wyde_1: u16 = mem[ WydeAt(10)].into();
+        let  wyde_2: u16 = mem[ WydeAt(12)].into();
+        let  wyde_3: u16 = mem[ WydeAt(14)].into();
+        assert_eq!( wyde_0, 0xFF8F);
+        assert_eq!( wyde_1, 0x8848);
+        assert_eq!( wyde_2, 0x4424);
+        assert_eq!( wyde_3, 0x2212);
+
+        let tetra_0: u32 = mem[TetraAt( 8)].into();
+        let tetra_1: u32 = mem[TetraAt(12)].into();
+        assert_eq!(tetra_0, 0xFF8F_8848);
+        assert_eq!(tetra_1, 0x4424_2212);
+
+        let  octa  : u64 = mem[ OctaAt( 8)].into();
+        assert_eq!( octa  , 0xFF8F_8848_4424_2212);
+
+        // Check if anything else is zero
+        for i in (0..8).chain(16..24) {
+            let zero: u8 = mem[ByteAt(i)].into();
+            assert_eq!(zero, 0);
+        }
+    }
+}
