82c57fc69bdf4b2559a474accfce01ac002568ca
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-02-16 20:49:01 +0100

    Abuse TRAP instruction to implement a simple HALT command

--- src/machine/behavior/is/interrupt/trap.rs
+++ src/machine/behavior/is/interrupt/trap.rs
@@ -1,6 +1,7 @@
 use machine::state::State;
 
 pub fn trap(_state: &mut State, _x: u8, _y: u8, _z: u8) {
-    unimplemented!();
+    // FIXME For the moment, TRAP halts the machine
+    println!("Goodbye!");
+    ::std::process::exit(0);
 }
-
