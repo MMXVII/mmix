ce3f6f97e6686957ca3098ab59439ca30cab38cb
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-02-16 10:26:39 +0100

    Added struct Config (with some basic methods) to the module cli

--- src/cli.rs
+++ src/cli.rs
@@ -1,1 +1,21 @@
+pub struct Config {
+    filename: String,
+    step: bool,
+}
 
+impl Config {
+    pub fn parse() -> Self {
+        // TODO
+        unimplemented!();
+    }
+
+    pub fn filename(&self) -> &str {
+        // TODO
+        unimplemented!();
+    }
+
+    pub fn step(&self) -> bool {
+        // TODO
+        unimplemented!();
+    }
+}
