4fec34d2c51e628d209c277cab0a85135d4484ce
Tobias Stolzmann <tobias.stolzmann@gmail.com>
2017-02-16 16:43:07 +0100

    Whitespace

--- src/machine/behavior/cu.rs
+++ src/machine/behavior/cu.rs
@@ -2,7 +2,6 @@ use machine::state::State;
 use machine::state::mem::ByteAt;
 
 pub fn cycle(state: &mut State) {
-
     // The program counter in state may point to any of the four bytes in the
     // tetrabyte that is to be executed next.
     // We compute the address of the first byte of the next instruction, which stores
@@ -10,10 +9,10 @@ pub fn cycle(state: &mut State) {
     let opcode_address = state.pc & !(2 + 1);
 
     // Fetch opcode and operands
-    let opcode: u8 = state.mem[ByteAt(opcode_address)].into();
-    let x: u8 = state.mem[ByteAt(opcode_address + 1)].into();
-    let y: u8 = state.mem[ByteAt(opcode_address + 2)].into();
-    let z: u8 = state.mem[ByteAt(opcode_address + 3)].into();
+    let opcode: u8 = state.mem[ByteAt(opcode_address    )].into();
+    let x: u8      = state.mem[ByteAt(opcode_address + 1)].into();
+    let y: u8      = state.mem[ByteAt(opcode_address + 2)].into();
+    let z: u8      = state.mem[ByteAt(opcode_address + 3)].into();
 
     // Get instruction implementation corresponding to opcode
     let instruction = super::is::get_semantics(opcode);
